6ee6c5f94c0c8312836aaf54df2e93fb1843f1af
rehanulhb <rehanulh@gmail.com>
2025-03-01 23:04:43 +0100

    Database Connection Done with Creating my IP Address Whitelisted

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
-
-
 app.use(cors());
 app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.lsofw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lsofw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,21 +25,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const menuCollection = client.db("restaurentDB").collection("menu");
+
+    app.get('/menu', async(req, res)=>{
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res)=>{
-    res.send('Server is Running')
-})
+  res.send('Server is Running')
+});
 
 app.listen(port, ()=>{
-    console.log(`Bistro Boss is Running on Port ${port}`)
+  console.log(`Restaurent is Running on Port ${port}`);
 })
