6ed9263fa44317e345e2203e8b1d0cb4c19cc0c3
rehanulhb <rehanulh@gmail.com>
2025-05-14 02:00:11 +0200

    Installed JSON Web Token and Edited Delete API - Done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -58,7 +59,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/users/:id", async () => {
+    app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
