f8181546e4b5154f35df327dd23735e99fbeec27
rehanulhb <rehanulh@gmail.com>
2025-05-19 00:33:51 +0200

    Verify Token Created for Verify the Token

--- index.js
+++ index.js
@@ -42,10 +42,17 @@ async function run() {
     //Middlewares
     const verifyToken = (req, res, next) => {
       console.log("Inside Verify Token", req.headers);
-      if (req.headers.authorization) {
+      if (!req.headers.authorization) {
         return res.status(401).send({ message: "Forbidden Access" });
       }
-      //next();
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
     };
 
     //User Related API
